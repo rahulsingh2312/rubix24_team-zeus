4e97691334f6e85d043876247a989b4a2f0ed80a
Alstudd <alstonsoares17@gmail.com>
2024-01-17 22:25:35 +0530

    Nav and Carousel starter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,6 @@ import CarouselSection from "./components/CarouselSection";
 import Header from "./components/Header";
 import { useState } from "react";
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="">
       <Header />
---
+++ frontend/src/data/sliderDetails.js
@@ -0,0 +1,68 @@
+const sliderDetails = [
+    {
+        title: "Beach walk badung",
+        place: "Bali, Indonesia",
+        rating: 8.4,
+        review: 636,
+        price: 498,
+        imgUrl: "https://images.unsplash.com/32/Mc8kW4x9Q3aRR3RkP5Im_IMG_4417.jpg?ixid=M3wxMjA3fDB8MXxzZWFyY2h8MTZ8fGJhY2tncm91bmQlMjBpbWFnZXxlbnwwfHx8fDE3MDU0NzA4MTZ8MA&ixlib=rb-4.0.3"
+    },
+    {
+        title: "Beach walk badung",
+        place: "Bali, Indonesia",
+        rating: 8.4,
+        review: 636,
+        price: 498,
+        imgUrl: "https://images.unsplash.com/32/Mc8kW4x9Q3aRR3RkP5Im_IMG_4417.jpg?ixid=M3wxMjA3fDB8MXxzZWFyY2h8MTZ8fGJhY2tncm91bmQlMjBpbWFnZXxlbnwwfHx8fDE3MDU0NzA4MTZ8MA&ixlib=rb-4.0.3"
+    },
+    {
+        title: "Beach walk badung",
+        place: "Bali, Indonesia",
+        rating: 8.4,
+        review: 636,
+        price: 498,
+        imgUrl: "https://images.unsplash.com/32/Mc8kW4x9Q3aRR3RkP5Im_IMG_4417.jpg?ixid=M3wxMjA3fDB8MXxzZWFyY2h8MTZ8fGJhY2tncm91bmQlMjBpbWFnZXxlbnwwfHx8fDE3MDU0NzA4MTZ8MA&ixlib=rb-4.0.3"
+    },
+    {
+        title: "Beach walk badung",
+        place: "Bali, Indonesia",
+        rating: 8.4,
+        review: 636,
+        price: 498,
+        imgUrl: "https://images.unsplash.com/32/Mc8kW4x9Q3aRR3RkP5Im_IMG_4417.jpg?ixid=M3wxMjA3fDB8MXxzZWFyY2h8MTZ8fGJhY2tncm91bmQlMjBpbWFnZXxlbnwwfHx8fDE3MDU0NzA4MTZ8MA&ixlib=rb-4.0.3"
+    },
+    {
+        title: "Beach walk badung",
+        place: "Bali, Indonesia",
+        rating: 8.4,
+        review: 636,
+        price: 498,
+        imgUrl: "https://images.unsplash.com/32/Mc8kW4x9Q3aRR3RkP5Im_IMG_4417.jpg?ixid=M3wxMjA3fDB8MXxzZWFyY2h8MTZ8fGJhY2tncm91bmQlMjBpbWFnZXxlbnwwfHx8fDE3MDU0NzA4MTZ8MA&ixlib=rb-4.0.3"
+    },
+    {
+        title: "Beach walk badung",
+        place: "Bali, Indonesia",
+        rating: 8.4,
+        review: 636,
+        price: 498,
+        imgUrl: "https://images.unsplash.com/32/Mc8kW4x9Q3aRR3RkP5Im_IMG_4417.jpg?ixid=M3wxMjA3fDB8MXxzZWFyY2h8MTZ8fGJhY2tncm91bmQlMjBpbWFnZXxlbnwwfHx8fDE3MDU0NzA4MTZ8MA&ixlib=rb-4.0.3"
+    },
+    {
+        title: "Beach walk badung",
+        place: "Bali, Indonesia",
+        rating: 8.4,
+        review: 636,
+        price: 498,
+        imgUrl: "https://images.unsplash.com/32/Mc8kW4x9Q3aRR3RkP5Im_IMG_4417.jpg?ixid=M3wxMjA3fDB8MXxzZWFyY2h8MTZ8fGJhY2tncm91bmQlMjBpbWFnZXxlbnwwfHx8fDE3MDU0NzA4MTZ8MA&ixlib=rb-4.0.3"
+    },
+    {
+        title: "Beach walk badung",
+        place: "Bali, Indonesia",
+        rating: 8.4,
+        review: 636,
+        price: 498,
+        imgUrl: "https://images.unsplash.com/32/Mc8kW4x9Q3aRR3RkP5Im_IMG_4417.jpg?ixid=M3wxMjA3fDB8MXxzZWFyY2h8MTZ8fGJhY2tncm91bmQlMjBpbWFnZXxlbnwwfHx8fDE3MDU0NzA4MTZ8MA&ixlib=rb-4.0.3"
+    },
+];
+
+export default sliderDetails;
