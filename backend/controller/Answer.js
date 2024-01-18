7711e5f1a196f0df91380603435b62e06cd42130
vikrantsingh22 <vikrantsingh2020004@gmail.com>
2024-01-18 23:27:37 +0530

    Added Answer listing feature

--- backend/controller/Answer.js
+++ backend/controller/Answer.js
@@ -4,7 +4,7 @@ const QuestionModel = require("../models/Questions");
 async function addAnswer(req, res) {
   const { answerString, questionId } = req.body;
   const user_id = req.user.id;
-
+  let updatedQuestion;
   const availableQuestion = await QuestionModel.findOne({ _id: questionId });
   if (availableQuestion) {
     try {
@@ -13,8 +13,13 @@ async function addAnswer(req, res) {
         answerString,
         questionId,
       });
-
       console.log(newAnswer.answerString);
+      updatedQuestion = await QuestionModel.findOneAndUpdate(
+        {
+          _id: questionId,
+        },
+        { $push: { answer_id: newAnswer.id } }
+      );
     } catch (e) {
       console.log(`Encounterd a error while adding the answer ${e}`);
     }
@@ -28,7 +33,9 @@ async function listAnswer(req, res) {
   const availableQuestion = await QuestionModel.find({ _id: questionId });
   if (availableQuestion) {
     try {
-      const answerList = await answerModel.find({ questionId: questionId });
+      const answerList = await answerModel
+        .find({ questionId: questionId })
+        .populate({ path: "user_id" });
       console.log(`Answer list : ${answerList}`);
       res.json({ res: answerList });
     } catch (e) {
