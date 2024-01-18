7716780773933fd116f2753eb1e3d7224b202e82
rizzabh <pandeyrishabh966@gmail.com>
2024-01-18 11:48:40 +0530

    Hero Tweaks

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import CarouselSection from "./components/CarouselSection";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Why from "./components/Why";
 function App() {
   return (
     <div className="">
       <Header />
       <Hero />
+      <Why />
       <CarouselSection />
     </div>
   );
