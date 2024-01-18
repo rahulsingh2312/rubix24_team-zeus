b1fcd180f4a9b100127926eda7396228868a69a3
Alstudd <alstonsoares17@gmail.com>
2024-01-18 14:26:59 +0530

    Added Services

---
+++ frontend/src/data/serviceDetails.jsx
@@ -0,0 +1,21 @@
+import { MdAnalytics, MdSafetyCheck, MdBook } from 'react-icons/md'
+
+const ourServices = [
+    {
+        icon: <MdSafetyCheck />,
+        heading: "Security",
+        detail: "loreum ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt"
+    },
+    {
+        icon: <MdAnalytics />,
+        heading: "Analysis",
+        detail: "loreum ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt"
+    },
+    {
+        icon: <MdBook />,
+        heading: "User Registration",
+        detail: "Register and Login to your account to access your data"
+    },
+];
+
+export default ourServices;
