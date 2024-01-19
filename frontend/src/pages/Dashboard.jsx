09fb3f74e75c9b747a15c54bac07af8e8ab3f7cb
Rahul <rahulsinghhh2312@gmail.com>
2024-01-19 13:09:12 +0530

    deployment Dashboard.jsx

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -29,29 +29,7 @@ function Dashboard() {
     </div>
   );
 
-    const [username, setUsername] = useState("");
-    const [loading, setLoading] = useState(false);
-    useEffect(() => {
-        setLoading(true);
-        axios
-        .get(`http://localhost:5555/User/current`)
-        .then((response) => {
-            console.log(response.data);
-            setUsername(response.data);
-            setLoading(false);
-        })
-        .catch((error) => {
-            console.log(error);
-            setLoading(false);
-        });
-    }, []);
-    return (
-        <div>
-            <DashboardHeader />
-            {/* {loading ? <Spinner /> : ""} */}
-            {/* Welcome, {username} */}
-        </div>
-    )
+    
 
 }
 
